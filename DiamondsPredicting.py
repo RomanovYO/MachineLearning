f0ffbe0f3a621a11366cfe6b51930667d32ee3d5
RomanovYO <94523600+RomanovYO@users.noreply.github.com>
2021-11-21 21:33:50 +0200

    Расчет отклонения, построение графика

--- DiamondsPredicting.py
+++ DiamondsPredicting.py
@@ -27,17 +27,32 @@ x = df[['carat','cut','color','clarity','depth','table','x','y','z']]
 y = df[['price']]
 
 # Разделение данных на тренировочный и тестовый наборы
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 15, random_state = 101)
+test_size = 25
+random_state = 202
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = test_size, random_state = random_state)
 
 # Тренировка
-regr = RandomForestRegressor(n_estimators = 10, max_depth = 10, random_state = 101)
+regr = RandomForestRegressor(n_estimators = 10, max_depth = 10, random_state = random_state)
 regr.fit(x_train, y_train.values.ravel())
 
 # Прогнозрование
 predictions = regr.predict(x_test)
 
-result = x_test
-result['price'] = y_test
+result               = x_test
+result['price']      = y_test
 result['prediction'] = predictions.tolist()
-
+result['deviation']  = 100 * (result['price'] - result['prediction']) / result['price']
 print(result.to_string())
+print('deviation min,max: ', '{:.2f}'.format(min(abs(result['deviation']))), '{:.2f}'.format(max(abs(result['deviation']))))
+
+# Определение оси X
+x_axis = x_test.carat
+
+# Построение графика
+plt.scatter(x_axis, y_test,      c = 'b', alpha = 0.5, marker = '.', label = 'Real')
+plt.scatter(x_axis, predictions, c = 'r', alpha = 0.5, marker = '.', label = 'Predicted')
+plt.xlabel('Carat')
+plt.ylabel('Price')
+plt.grid(color = '#d3d3d3', linestyle = 'solid')
+plt.legend(loc = 'lower right')
+plt.show()
